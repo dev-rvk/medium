7b22ccc26f50ead8aa9c10674f26151703b98568
Raghav Korde <raghavkorde4@gmail.com>
2024-07-24 12:15:17 +0530

    fixed avatar

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -15,15 +15,15 @@ export const BlogCard = (postData : BlogCardInput) => {
         <div className="w-full cursor-pointer">
             <Link to={`/blog/${postData.id}`}>
                 <div className="flex flex-col px-3 py-3 w-full mt-4 mb-1 border-2 rounded-lg shadow-lg">
-                    <div className="flex flex-row items-center">
-                        <div className="flex flex-col justify-center"><Avatar size="small" text={postData.authorName[0]} /></div>
+                    <div className="flex flex-row items-center mt-2">
+                        <div className="ml-4"> <Avatar  size="large" text={postData.authorName[0]} /> </div>
                         <div className="ml-3 mr-3 text-lg font-semibold">{postData.authorName}</div>
                         <Dot />
                         <div className="mr-3 font-extralight text-slate-600">{postData.date}</div>
                     </div>
-                    <div className="mt-4 font-black text-2xl"> {postData.title} </div>
-                    <div className="mt-2 font-extralight text-xl text-gray-500"> {postData.content.substring(0, 150) + (postData.content.length < 100 ? "" : "...")} </div>
-                    <div className="mt-6 font-extralight text-gray-400"> { minsRead + (minsRead > 1 ? " minutes read" : " minute read") } </div>
+                    <div className="ml-4 mt-4 font-black text-2xl"> {postData.title} </div>
+                    <div className="ml-4 mt-2 font-extralight text-xl text-gray-500"> {postData.content.substring(0, 150) + (postData.content.length < 100 ? "" : "...")} </div>
+                    <div className="ml-4 mt-6 font-extralight text-gray-400"> { minsRead + (minsRead > 1 ? " minutes read" : " minute read") } </div>
                 </div>
             </Link>
         </div>
@@ -33,7 +33,7 @@ export const BlogCard = (postData : BlogCardInput) => {
 
 
 export const Avatar = ({ size, text }: { size: 'small' | 'large'; text: string }) => {
-    const dim = size === 'large' ? 10 : 8
+    const dim = size === 'small' ? 8 : 10
     return (
         <div className={`relative inline-flex items-center justify-center w-${dim} h-${dim} overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
             <span className="font-medium text-gray-600 dark:text-gray-300">{text[0]}</span>
